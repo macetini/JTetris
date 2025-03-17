bab09faf2355e0d7992be431c5e7dcc053dacc49
Marko Cetinic <Marko Cetinic@KOMP.mshome.net>
2025-03-17 11:17:15 +0100

    Added new color and new Shape

--- src/meta/Colors.java
+++ src/meta/Colors.java
@@ -6,8 +6,11 @@ public class Colors {
 													//TIMBERWOLF GRAY
 	public static final Color BACKGROUND_COLOR = new Color(219,215,210);
 	
-													//LIGHTSABRE BLUE		//GRASS GREEN			//DARK YELLOW			//BURNT ORANGE			//AMARANTH RED
-	public static final Color[] SHAPE_COLORS = { new Color(46,103,248), new Color(0, 154, 23), new Color(246, 190, 0), new Color(203, 96, 21), new Color(244, 54, 76) };
+													//LIGHTSABRE BLUE		//GRASS GREEN			//DARK YELLOW
+	public static final Color[] SHAPE_COLORS = { new Color(46,103,248), new Color(0, 154, 23), new Color(246, 190, 0),
+			
+													//BURNT ORANGE			//AMARANTH RED			//PLUM PURPLE			
+												 new Color(203, 96, 21), new Color(244, 54, 76), new Color(156,80,182) };
 	
 	public static final Color SHAPE_RECT_COLOR = Color.BLACK;
 	
