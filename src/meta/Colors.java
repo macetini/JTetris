283d5cd9446a8274d7215fe3c28286e919e476e5
Marko Cetinic <Marko Cetinic@KOMP.mshome.net>
2025-03-17 11:47:30 +0100

    Small refactoring

--- src/meta/Colors.java
+++ src/meta/Colors.java
@@ -10,7 +10,7 @@ public class Colors {
 	public static final Color[] SHAPE_COLORS = { new Color(46,103,248), new Color(0, 154, 23), new Color(246, 190, 0),
 			
 													//BURNT ORANGE			//AMARANTH RED			//PLUM PURPLE			
-												 new Color(203, 96, 21), new Color(244, 54, 76), new Color(156,80,182) };
+												 new Color(203, 96, 21), new Color(244, 54, 76), new Color(156, 80, 182) };
 	
 	public static final Color SHAPE_RECT_COLOR = Color.BLACK;
 	
